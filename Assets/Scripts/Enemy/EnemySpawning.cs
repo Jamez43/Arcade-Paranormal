54248c41d4c4f403a84b323033c53d6684358aa5
Jamez43 <jameswyllie42@gmail.com>
2025-11-08 23:23:51 -0500

    Changed build setttings

--- Assets/Scripts/Enemy/EnemySpawning.cs
+++ Assets/Scripts/Enemy/EnemySpawning.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Rendering.Universal;
 using UnityEngine;
 
 public class EnemySpawning : MonoBehaviour
